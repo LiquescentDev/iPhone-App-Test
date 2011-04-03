27d6a099c61be03a6460137ac4c8e1cf14a85619
Richard Kiene <richard@liquescentdevelopment.com>
2011-04-03 09:39:12 -0700

    Initial Test App Commit

---
+++ iPhone Testing/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  iPhone Testing
+//
+//  Created by Richard Kiene on 4/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ iPhone Testing/iPhone_TestingAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  iPhone_TestingAppDelegate.h
+//  iPhone Testing
+//
+//  Created by Richard Kiene on 4/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iPhone_TestingAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
